e740cc95f1aca2bea05a655d33ed33ffbb305511
Benjamin Kobjolke <b.kobjolke@xida.de>
2023-12-04 21:59:35 +0100

    FEATURE: first working version

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+import ctypes
 import os
 import sys
 import time
-from dateutil import parser
+
+# Define a structure for the last input info (for Windows)
+class LASTINPUTINFO(ctypes.Structure):
+    _fields_ = [("cbSize", ctypes.c_uint),
+                ("dwTime", ctypes.c_uint)]
 
 def get_idle_time():
-    # This function needs to be implemented based on the operating system.
-    # For Windows, you can use ctypes to call the GetLastInputInfo from User32.dll
-    # For Linux and MacOS, different methods will be required.
-    pass
+    lii = LASTINPUTINFO()
+    lii.cbSize = ctypes.sizeof(LASTINPUTINFO)
+    ctypes.windll.user32.GetLastInputInfo(ctypes.byref(lii))
+    millis = ctypes.windll.kernel32.GetTickCount() - lii.dwTime
+    return millis / 1000.0  # Convert milliseconds to seconds
 
 def shutdown():
-    os.system("shutdown /s /f /t 0")  # Force shutdown command for Windows
-    # For Linux, use 'shutdown now -h'
-    # For MacOS, use 'shutdown -h now'
+    os.system("shutdown /s /f /t 0")
+
+def format_time(seconds):
+    mins, secs = divmod(int(seconds), 60)
+    return f"{mins} minutes, {secs} seconds"
 
 def main():
     input_time = input("Enter time to wait before shutdown (e.g., 60m for 60 minutes, 2h for 2 hours): ")
@@ -28,13 +36,28 @@ def main():
         print("Invalid number.")
         sys.exit(1)
 
-    print(f"System will shutdown after {input_time} of idle time.")
+    print(f"System will shutdown after {input_time} of idle time if no activity is detected.")
+    start_time = time.time()
+
     while True:
-        idle_time = get_idle_time()
-        if idle_time >= wait_time:
+        current_idle_time = get_idle_time()
+
+        # If there is user activity, reset the start_time
+        if current_idle_time < 2:  # Using 2 seconds to allow for small delays in detection
+            start_time = time.time()
+
+        elapsed_time = time.time() - start_time
+        remaining_time = max(wait_time - elapsed_time, 0)
+
+        formatted_time = format_time(remaining_time)
+        print(f"Time remaining until shutdown: {formatted_time}.")
+        #print(f"Current idle time: {format_time(current_idle_time)}.")
+
+        if remaining_time <= 0:
             shutdown()
             break
-        time.sleep(60)  # Check every minute
+
+        time.sleep(1)  # Check every second
 
 if __name__ == "__main__":
     main()
